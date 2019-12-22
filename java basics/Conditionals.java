f35da817d7c19b28a7134b913df4d3ac7bc9e233
Mubariz Khan <37043727+MubarizKhan@users.noreply.github.com>
2019-12-22 06:52:04 +0500

    Add files via upload

---
+++ java basics/Conditionals.java
@@ -0,0 +1,22 @@
+package mak;
+
+public class Conditionals
+{
+    void cond() {
+        int var1 = 5;
+        int var2 = 3;
+        if(var1 > var2)
+        {
+            System.out.println("Var1 is greater than var2");
+        }
+        else{
+            System.out.println("var2 is less");
+
+        }
+        System.out.println("This is th lain dile");
+
+
+    }
+}
+
+
