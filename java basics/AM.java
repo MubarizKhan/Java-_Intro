b02728793ca6703dde459667f1c37448fa8b779c
Mubariz Khan <37043727+MubarizKhan@users.noreply.github.com>
2019-12-22 06:53:23 +0500

    Add files via upload

---
+++ java basics/AM.java
@@ -0,0 +1,15 @@
+package mak;
+
+public class AM {
+//    Access Modifiers
+    private int val = 10;
+    public int getVal()
+    {
+        return val;
+    }
+    public void set_val(int val1)
+    {
+        val = val1;
+    }
+
+}
